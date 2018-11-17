b824f5a67bb3c3c503b4422aa5d28131f374e0ab
seattleowl <seattleowl@users.noreply.github.com>
2018-11-17 12:48:40 -0800

    GS framework

---
+++ main.js
@@ -0,0 +1,15 @@
+// Canvas
+var canvas = new GameSquare.GameSquareCanvasObject("canvas")
+
+// Sprites
+var player = new GameSquare.GameSquareRectObject({
+    x: 10,
+    y: 10,
+    width: 10,
+    height: 10
+})
+
+// Loop
+GameSquare.updateLoopCallback = function () {
+    
+}
