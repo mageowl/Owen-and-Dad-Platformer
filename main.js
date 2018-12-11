d4c7f53dbebfca3c61314b942c6efa99949f6bbb
seattleowl <seattleowl@users.noreply.github.com>
2018-12-11 06:58:55 -0800

    Player Class

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
 // Canvas
 var canvas = new GameSquare.GameSquareCanvasObject("canvas")
 
-// Sprites
-var player = new GameSquare.GameSquareRectObject({
-    x: 10,
-    y: 10,
-    width: 10,
-    height: 10,
-    color: "dodgerblue"
-})
+// Classes
+class Player extends GameSquare.GameSquareRectObject {
+    constructer(x, y) {
+        // Super
+        super({
+            x: x,
+            y: y,
+            width: 10,
+            height: 10,
+            color: "dodgerblue"
+        })
+    }
+}
 
+// Sprites
+
+// var player = new GameSquare.GameSquareRectObject({
+//     x: 10,
+//     y: 10,
+//     width: 10,
+//     height: 10,
+//     color: "dodgerblue"
+// })
 // Loop
 GameSquare.updateLoopCallback = function () {
-    player.render()
+    //player.render()
 }
